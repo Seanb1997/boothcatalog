f8ba3f1810c11d49c0e7536c60c59364a1decdf6
Seanb1997 <sean.baker260697@gmail.com>
2026-02-24 19:38:13 +0000

    Create page.tsx

---
+++ app/brief/page.tsx
@@ -0,0 +1,11 @@
+import BriefForm from '../../components/BriefForm';
+import Header from '../../components/Header';
+
+export default function BriefPage() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      <BriefForm />
+    </div>
+  );
+}
